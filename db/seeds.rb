4ba3d29b24436c29a3a4d287ed26220d012eb657
Zane Hooper <zanehoop@gmail.com>
2021-07-19 00:56:39 +0000

    Generate TODO scaffolding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.create(name: "First todo")
+Todo.create(name: "Second todo")
+Todo.create(name: "Third todo")
